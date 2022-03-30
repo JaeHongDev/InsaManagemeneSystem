8a8696ee4d03a3dd4d243d10801fbb43d78c76fd
JaehongDev <println@kakao.com>
2022-03-30 22:24:58 +0900

    Create CodeModelTest.cs

--- InsaManagementSystem/CodeGroup/CodeGroupModel.cs
+++ InsaManagementSystem/CodeGroup/CodeGroupModel.cs
@@ -11,53 +11,76 @@ namespace InsaManagementSystem.CodeGroup
 {
     public class CodeGroupModel
     {
-        [Column("CDG_GRPCD")] public string CdgGrpcd { get; set; }
+        private string _cdgCrpcd;
+
+        /// <summary>
+        ///  임시 값
+        /// </summary>
+        public string CdgGrpcdTemp { get; set; }
+        //[Column("CDG_GRPCD")] public string CdgGrpcd { get; set; }
         [Column("CDG_GRPNM")] public string CdgGrpnm { get; set; }
         [Column("CDG_DIGIT")] public int CdgDigit { get; set; }
         [Column("CDG_LENGTH")] public int CdgLength { get; set; }
         [Column("CDG_USE")] public string CdgUse { get; set; }
         [Column("CDG_KIND")] public string CdgKind { get; set; }
 
+        [Column("CDG_GRPCD")]
+        public string CdgGrpcd
+        {
+            get => _cdgCrpcd;
+            set
+            {
+                _cdgCrpcd = value;
+                if (CdgGrpcdTemp is null) CdgGrpcdTemp = value;
+            }
+        }
         public int Insert()
         {
-            if(findById(this) is null) return SqlMapperHelper.ExecuteQuery(CodeGroupSql.Insert, this);
+            if (FindById(this) is null) return SqlMapperHelper.ExecuteQuery(CodeGroupSql.Insert, this);
             throw new CommonException(ErrorDictionary.EXISTS_DATA());
         }
-
         public static void CleanUp()
         {
             SqlMapperHelper.ExecuteQuery(CodeGroupSql.DeleteAll);
         }
-
         public static List<CodeGroupModel> getAll()
         {
             return SqlMapperHelper.Query<CodeGroupModel>(CodeGroupSql.GetAll).ToList();
         }
-
         private bool Equals(CodeGroupModel other)
         {
             return CdgGrpcd == other.CdgGrpcd && CdgGrpnm == other.CdgGrpnm && CdgDigit == other.CdgDigit &&
                    CdgLength == other.CdgLength && CdgUse == other.CdgUse && CdgKind == other.CdgKind;
         }
-
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
             return obj.GetType() == GetType() && Equals((CodeGroupModel) obj);
         }
-
-        public static CodeGroupModel findById(CodeGroupModel codeGroupModel)
+        public static CodeGroupModel FindById(CodeGroupModel codeGroupModel)
         {
-            return SqlMapperHelper.Query<CodeGroupModel>("SELECT * FROM TINSA.TINSA_CDG WHERE CDG_GRPCD = :CDG_CROPCD",
-                new {CDG_CROPCD = codeGroupModel.CdgGrpcd}).FirstOrDefault();
+            return FindById(codeGroupModel.CdgGrpcd);
         }
-
+        private static CodeGroupModel FindById(string id)
+        {
+            var result = SqlMapperHelper.Query<CodeGroupModel>(CodeGroupSql.FindById,
+                new {CDG_CROPCD = id}).ToList();
+            return result.Count == 0 ? null : result[0];
+        }
         public void ToggleUse()
         {
-            CdgUse = CdgUse.Equals("Y") ? "N" : "Y";
-            SqlMapperHelper.ExecuteQuery(CodeGroupSql.ToggleUse,this);
-        }
+            var test = FindById(this);
+            if (test is null) throw new CommonException(ErrorDictionary.DID_NOT_EXISTS_DATA());
 
+            CdgUse = CdgUse.Equals("Y") ? "N" : "Y";
+            SqlMapperHelper.ExecuteQuery(CodeGroupSql.ToggleUse, this);
+        }
+        public void Update()
+        {
+            if (FindById(CdgGrpcdTemp) is null) throw new CommonException(ErrorDictionary.DID_NOT_EXISTS_DATA());
+            if (!(FindById(CdgGrpcd) is null)) throw new CommonException(ErrorDictionary.EXISTS_DATA());
+            SqlMapperHelper.ExecuteQuery(CodeGroupSql.Update, this);
+        }
     }
 }
--- InsaManagementSystem/CodeGroup/CodeGroupModelTest.cs
+++ InsaManagementSystem/CodeGroup/CodeGroupModelTest.cs
@@ -1,6 +1,6 @@
-using System.CodeDom;
 using InsaManagementSystem.Util;
 using NUnit.Framework;
+using static InsaManagementSystem.Util.ErrorDictionary;
 
 namespace InsaManagementSystem.CodeGroup
 {
@@ -12,7 +12,6 @@ namespace InsaManagementSystem.CodeGroup
         {
             CodeGroupModel.CleanUp();
         }
-
         [Test]
         public void 코드그룹모델생성테스트()
         {
@@ -27,7 +26,6 @@ namespace InsaManagementSystem.CodeGroup
             var result = codeGroupModel.Insert();
             Assert.AreEqual(result, 1);
         }
-
         [Test]
         public void 코드그룹모델전체값가져오기()
         {
@@ -44,7 +42,6 @@ namespace InsaManagementSystem.CodeGroup
             var expected = CodeGroupModel.getAll()[0];
             Assert.IsTrue(codeGroupModel.Equals(expected));
         }
-
         [Test]
         public void 코드그룹모델에서아이디값으로값찾아오기()
         {
@@ -58,11 +55,9 @@ namespace InsaManagementSystem.CodeGroup
                 CdgLength = 2
             };
             codeGroupModel.Insert();
-            var expected = CodeGroupModel.findById(codeGroupModel);
+            var expected = CodeGroupModel.FindById(codeGroupModel);
             Assert.IsTrue(expected.Equals(codeGroupModel));
         }
-
-
         [Test]
         public void 코드그룹모델데이터입력에서중복된값이들어갈경우에러발생시키기()
         {
@@ -80,9 +75,23 @@ namespace InsaManagementSystem.CodeGroup
                 codeGroupModel.Insert();
                 codeGroupModel.Insert();
             }).GetMessage();
-            Assert.True(errorMessage.Equals(ErrorDictionary.EXISTS_DATA()));
+            Assert.True(errorMessage.Equals(EXISTS_DATA()));
         }
-
+        [Test]
+        public void 코드그룹모델의값사용여부변경할때데이터가존재하지않는경우오류발생시키기()
+        {
+            var codeGroupModel = new CodeGroupModel
+            {
+                CdgDigit = 1,
+                CdgGrpcd = "001",
+                CdgGrpnm = "코드그룹테슽",
+                CdgKind = "테스트",
+                CdgUse = "Y",
+                CdgLength = 2
+            };
+            var errorMessage = Assert.Throws<CommonException>(() => { codeGroupModel.ToggleUse(); });
+            Assert.IsTrue(errorMessage.GetMessage().Equals(DID_NOT_EXISTS_DATA()));
+        }
         [Test]
         public void 코드그룹모델의값사용여부변경하기()
         {
@@ -97,14 +106,12 @@ namespace InsaManagementSystem.CodeGroup
             };
             codeGroupModel.Insert();
             codeGroupModel.ToggleUse();
-            Assert.AreEqual(codeGroupModel.CdgUse, CodeGroupModel.findById(codeGroupModel).CdgUse);
+            Assert.AreEqual(codeGroupModel.CdgUse, CodeGroupModel.FindById(codeGroupModel).CdgUse);
         }
-
-        [Ignore("업데이트 방식이 정해지지 않아 처리하지 않습니다.")]
         [Test]
         public void 코드그룹모델의값없데이트하기()
         {
-            CodeGroupModel codeGroupModel = new CodeGroupModel
+            var codeGroupModel = new CodeGroupModel
             {
                 CdgDigit = 1,
                 CdgGrpcd = "001",
@@ -114,6 +121,47 @@ namespace InsaManagementSystem.CodeGroup
                 CdgLength = 2
             };
             codeGroupModel.Insert();
+
+            var expectedCodeGroupModel = CodeGroupModel.FindById(codeGroupModel);
+            
+            expectedCodeGroupModel.CdgGrpcd = "002";
+            expectedCodeGroupModel.Update();
+
+            var expected = CodeGroupModel.FindById(expectedCodeGroupModel);
+            Assert.IsTrue(expected.Equals(expectedCodeGroupModel));
+        }
+        [Test]
+        public void 코드그룹모델의값업데이트할경우이미존재하는아이디에러발생시키기()
+        {
+            var codeGroupModel = new CodeGroupModel
+            {
+                CdgDigit = 1,
+                CdgGrpcd = "001",
+                CdgGrpnm = "코드그룹테슽",
+                CdgKind = "테스트",
+                CdgUse = "Y",
+                CdgLength = 2
+            };
+            codeGroupModel.Insert();
+            
+            var expectedCodeGroupModel = CodeGroupModel.FindById(codeGroupModel);
+            expectedCodeGroupModel.CdgGrpcd = "002";
+            
+            var codeGroupModel1 = new CodeGroupModel
+            {
+                CdgDigit = 1,
+                CdgGrpcd = "002",
+                CdgGrpnm = "코드그룹테슽",
+                CdgKind = "테스트",
+                CdgUse = "Y",
+                CdgLength = 2
+            };
+            codeGroupModel1.Insert();
+            var expectedException = Assert.Throws<CommonException>(() =>
+            {
+                expectedCodeGroupModel.Update();
+            });
+            Assert.IsTrue(expectedException.GetMessage().Equals(EXISTS_DATA()));
         }
     }
 }
